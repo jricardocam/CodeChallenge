7057ea88d926f730762f1fcf41cf413bab9b545a
jricardocam <jricardocam@gmail.com>
2022-04-29 22:39:59 -0500

    Agregando el Server con express

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const Reader=require("./lib/utils/reader")
 const ExplorerService=require("./lib/services/ExplorerService");
 
 const listaExplorers = Reader.readJsonFile("visualpartners.json");
-
 console.log(ExplorerService.explorerlist(listaExplorers))

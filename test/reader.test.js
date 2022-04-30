70a71837638aa4760ae967c0f923e59b4abef155
jricardocam <jricardocam@gmail.com>
2022-04-29 22:14:21 -0500

    Agregando TDD de Reader

---
+++ test/reader.test.js
@@ -0,0 +1,8 @@
+const Reader=require("../lib/utils/reader")
+
+describe("Test para el Reader",()=>{
+    test("Comprobar que se lea el archivo visualpartners.jsob" ,()=>{
+        const listaExplorers = Reader.readJsonFile("visualpartners.json");
+        expect(listaExplorers).tobeDefinded
+    });
+});
